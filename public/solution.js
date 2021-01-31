f11c9e6ccf54ed66b637aa165707445935b68eb2
Doug Franklin <doug@dougfranklin.com>
2021-01-31 18:11:43 -0500

    Finally got it working

---
+++ public/solution.js
@@ -0,0 +1,189 @@
+const assignmentResult = Promise.all([
+  // All Users
+  fetch('/users', { method: 'get', headers: { 'Content-Type': 'application/json'}})
+    .then(user_response => user_response.json()),
+ 
+  // Correct Users
+  fetch('/users', { method: 'get', headers: { 'Content-Type': 'application/json'}})
+    .then(user_response => user_response.json())
+    .then((users) => {
+      const corrected_users = users.filter((individual_user) => {
+        if (!individual_user.state) {
+          individual_user.state = "PA"
+          return individual_user;
+        }
+      })
+      console.log('corrected_users', corrected_users);
+      return corrected_users;
+    })
+
+    // Connect users to hobbies
+
+    // I already have the users
+    // Fetch the hobbies?
+    // - correct them first?
+
+    .then((users) => {
+
+      // Uncorrected Hobbies
+      const hobbies = fetch('/hobbies', { method: 'get', headers: { 'Content-Type': 'application/json'}})
+        .then(hobby_response => hobby_response.json())
+
+      const corrected_hobbies = hobbies
+        .then((hobbies) => {
+          const c_hobbies = hobbies.filter((hobby) => {
+            if(!hobby.experience) {
+              switch(hobby.years_played) {
+                case 1:
+                  hobby.experience = 'beginner';
+                  break;
+                case 2:
+                  hobby.experience = 'advanced';
+                  break;
+                case 3:
+                  hobby.experience = 'expert';
+                  break;
+              }
+              return hobby;
+            }
+          })
+          return c_hobbies;
+        })
+        .then(hobbies => 
+          fetch('/updateHobbies', { method: 'post', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(hobbies)} )
+          .then(res => res.json())
+          .then(hobbies => {
+            console.log('posted hobbies', hobbies);
+            return hobbies;
+          })
+        )
+
+        // const posted_hobbies = corrected_hobbies
+        //   .then(hobbies =>
+        //     fetch('/updateHobbies', { method: 'post', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(hobbies)} )
+        //     .then(res => res.json())
+        //     .then(hobbies => {
+        //       console.log('posted hobbies', hobbies);
+        //       return hobbies;
+        //     })
+        //   )
+
+      console.log('users', users);
+      console.log('hobbies', hobbies);
+      console.log('corrected hobbies', corrected_hobbies);
+      // console.log('posted hobbies', posted_hobbies);
+
+      return users;
+    })
+
+    // If I needed to find all of the hobbies for a person, I would do something like
+    // users.map(user => {
+    //   user.hobbies = hobbies.filter(hobby => hobby.user_id == user.id);
+     
+    //   return user;
+    // })
+
+    // Post the corrected users
+    .then(users => fetch('/updateUsers',
+                          {
+                            method: 'post',
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify(users)
+                          })
+    .then(res => res.json())
+    .then(users => {
+      console.log('posted users', users);
+      return users;
+    })
+  )
+
+//   Potential array element goes here
+//   fetch('/hobbies', { method: 'get', headers: { 'Content-Type': 'application/json'}})
+//     .then(hobby_response => hobby_response.json())
+//     .then((hobbies) => {
+   
+//     const c_hobbies = hobbies.filter((hobby) => {
+ 
+//       if(!hobby.experience) {
+ 
+//         switch(hobby.years_played) {
+ 
+//           case 1:
+//             hobby.experience = 'beginner';
+//             break;
+//           case 2:
+//             hobby.experience = 'advanced';
+//             break;
+//           case 3:
+//             hobby.experience = 'expert';
+//             break;
+//         }
+//         return hobby;
+//       }
+ 
+//     })
+//     return c_hobbies;
+//   })
+//   .then(hobbies =>
+//     fetch('/updateHobbies', { method: 'post', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(hobbies)} )
+//     .then(res => res.json())
+//     .then(hobbies => {
+//       console.log('posted hobbies', hobbies);
+//       return hobbies;
+//     })
+//   )
+])
+
+console.log('assignmentResult', assignmentResult);
+
+// If I needed to find all of the hobbies for a person, I would do something like
+// users.map(user => {
+//   user.hobbies = hobbies.filter(hobby => hobby.user_id == user.id);
+ 
+//   return user;
+// })
+
+// let users = [
+//   {"id":2,"name":"Rob","age":40,"city":"Harrisburg","state":"Test","last_modified":""}
+// ];
+
+// let hobbies = [
+//   {"id":20,"user_id":2,"name":"golf","years_played":1,"experience":"expert","last_modified":""},
+//   {"id":41,"user_id":4,"name":"knitting","years_played":2,"experience":"beginner","last_modified":""}
+ 
+// ];
+
+// let favorites = [
+//   {"id":11,"user_id":1,"name":"Corlaline","type":"new","last_modified":""},
+//   {"id":22,"user_id":2,"name":"Pizza","type":"newer","last_modified":""},
+//   {"id":62,"user_id":6,"name":"Ice cream","type":"newest","last_modified":""},
+// ];
+
+// fetch('/updateUsers', { method: "post", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(users) })
+//   .then(res => res.json())
+//   .then(users => {
+//     let hey = document.createElement('div');
+//     hey.innerHTML = JSON.stringify(users);
+//     document.getElementById('main').appendChild(hey);
+
+//     return fetch('/updateHobbies', { method: "post", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(hobbies) })
+//   })
+//   .then(res => res.json())
+//   .then(hobbies => {
+//     let hey = document.createElement('div');
+//     hey.innerHTML = JSON.stringify(hobbies);
+//     document.getElementById('main').appendChild(hey);
+   
+//     return fetch('/updateFavorites', { method: "post", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(favorites) })
+//   })
+//   .then(res => res.json())
+//   .then(faves => {
+//     let hey = document.createElement('div');
+//     hey.innerHTML = JSON.stringify(faves);
+//     document.getElementById('main').appendChild(hey);
+//   })
+
+// // DO NOT DELETE THIS!  
+// function reset() {
+//   fetch('/reset');
+// }
--- server.js
+++ server.js
@@ -7,11 +7,74 @@ let data = require('./data');
 let initialData = JSON.parse(JSON.stringify(data));
 
 server.use(body_parser.json());
+server.use(express.static('public'));
 
-server.get('/', (req, res) => {
-  res.send(data);
+// server.get('/', (req, res) => {
+//   res.send(data);
+// });
+
+server.get('/users', (req, res) => {
+  res.json(data.users);
+});
+
+server.get('/hobbies', (req, res) => {
+  res.json(data.users);
+});
+
+server.get('/favorites', (req, res) => {
+  res.json(data.users);
+});
+
+server.post('/updateUsers', (req, res) => {  
+  const newUserData = req.body;
+
+  res.json(updateData('users', newUserData));
+});
+
+server.post('/updateHobbies', (req, res) => {  
+  const newHobbyData = req.body;
+
+  res.json(updateData('hobbies', newHobbyData));
+});
+
+server.post('/updateFavorites', (req, res) => {  
+  const newFavoriteData = req.body;
+
+  res.json(updateData('favorites', newFavoriteData));
+});
+
+server.get('/reset', (req, res) => {
+  data = initialData;
 });
 
 server.listen(port, () => {
-  console.log(`Server listening at port ${ port }`);
+  console.log(`Server listening at ${port}`);
 });
+
+function updateData(type, newData) {
+  let indexForError, updatedRecord;
+  let updatedData = [];
+  // users and hobbies are treated normally.  favorites will have the possibility of throwing an error
+  if (type == 'favorites') {
+    const count = newData.length;
+    indexForError = Math.floor(Math.random() * Math.floor(count));
+  }
+
+  newData.forEach((item, index) => {
+    let recordIndex = data[type].findIndex(datum => datum.id == item.id);
+
+    // if no record is found, throw an error
+    if (recordIndex == -1) throw new Error(`${type} does not contain a record for id ${item.id}`);
+
+    if (type == 'favorites' && index == indexForError) {
+      updatedRecord = { id: item.id, user_id: item.user_id, error: "could not be saved"};
+    } else {
+      item.last_modified = 'now';
+      updatedRecord = item;
+    }
+
+    updatedData.push(updatedRecord);
+  });
+
+  return updatedData;
+}
